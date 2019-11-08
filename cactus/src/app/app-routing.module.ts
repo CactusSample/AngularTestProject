81c33f700f330fbb4f82b1876dc97d21e1da32e1
Sharath Kumar K <sharath.k@telxsi.com>
2019-11-08 14:26:44 +0530

    added authguard to cart page

--- cactus/src/app/app-routing.module.ts
+++ cactus/src/app/app-routing.module.ts
@@ -18,7 +18,7 @@ const routes: Routes = [
     path: '', redirectTo:'login' ,pathMatch: 'full'
   },
   {
-    path:'cart', component: CartDetailsComponent
+    path:'cart', component: CartDetailsComponent,canActivate:[AuthGuard]
   }
 ];
 
